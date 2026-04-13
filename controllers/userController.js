111876913b21b225a09039526fa325a0cfac86a4
Kevin Rodrigues <a.kevinrodrigues@gmail.com>
2026-04-13 18:18:56 -0400

    MFA test

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,8 +7,8 @@ import nodemailer from 'nodemailer';
 const otpStore = {};
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,  // SSL na 465
     family: 4,
     auth: {
         user: process.env.EMAIL_USER,
